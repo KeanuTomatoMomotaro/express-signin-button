8b175c0407863b37425c26d8f3828edd8890d5e0
Keanu Nurherbyanto <keanu.razgriz@gmail.com>
2018-03-17 22:36:13 +0700

    started implementing Q library for promise style

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 require('dotenv').config()
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'project_guestbook'
+});
 
+// const {promisify} = require ('util');
+// var Promise = require('promise');
+// const getFilePromise = promisify(connection.query())
+const q = require('q')
+
+//get all comments with promise
+function getComments(query) {
+  var deferred = q.defer();
+
+  connection.query(query, function(err, res){
+    if(err) throw err
+    deferred.resolve(res)
+  })
+
+  return deferred.promise
+}
+
+//get comment for certain id with promise
+function getComment(query, id){
+  var deferred = q.defer();
+
+  connection.query(query, id, function(err, res){
+    if(err) throw err
+    deferred.resolve(res)
+  })
+
+  return deferred.promise
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Sign in to Google', google_client_id: process.env.GOOGLE_CLIENT_ID });
 });
+
+// //mysql fetch promises in action 
+router.get('/get-all-guest-comments', function(req, res, next){
+  getComment('SELECT * FROM guest_comments where id = ?', '1').then(function (result) {
+    getComments('SELECT * FROM guest_comments').then(function (result2){
+      res.send({
+        result1: result,
+        result2: result2
+      })
+    })
+  })
+})
 
 module.exports = router;
